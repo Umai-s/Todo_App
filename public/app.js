5952b5fb102b1fd77dc728a392604c15c9aeeccd
Umai-s <umaima.salem@gmail.com>
2020-07-26 05:25:10 -0700

    Now ready to commit

---
+++ public/app.js
@@ -0,0 +1,49 @@
+var list = document.getElementById("list");
+
+function addTodo() {
+    var todo_item = document.getElementById("todo-item")
+    // if (todo_item.value != "") {
+
+        //creating li tag with text node
+        var li = document.createElement('li'); //this tag created here because multiple li will be created
+        var liText = document.createTextNode(todo_item.value)
+        li.setAttribute("class", "list")
+        li.appendChild(liText);
+
+        //creating delete button
+        var delBtn = document.createElement("button");
+        var delText = document.createTextNode("DELETE");
+        delBtn.setAttribute("class", "btnn")
+        delBtn.setAttribute("onclick", "deleteItem(this)")
+        delBtn.appendChild(delText);
+
+        //creating edit button
+        var editBtn = document.createElement("button");
+        var editText = document.createTextNode("EDIT");
+        editBtn.appendChild(editText);
+        editBtn.setAttribute("class", "btnn")
+        editBtn.setAttribute("onclick", "editItem(this)")
+
+        li.appendChild(delBtn)
+        li.appendChild(editBtn)
+
+        list.appendChild(li);
+        todo_item.value = "";
+    // }
+    // else {
+    //     alert("Please enter something.")
+    // }
+}
+
+function deleteItem(e) {
+    e.parentNode.remove()
+}
+
+function deleteAll() {
+    list.innerHTML = ""
+}
+
+function editItem(e) {
+    var val = prompt("Enter updated value", e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue = val;
+}
